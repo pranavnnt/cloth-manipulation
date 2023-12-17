a6ed70447ed77ad0886e4bcf882e6f26413bda40
Pranav Thakkar <pnt8@cornell.edu>
2023-12-17 01:30:47 -0500

    Fixed compile errors (build failing)

--- src/full_grasping_planner.cpp
+++ src/full_grasping_planner.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 
 #include <pcl_ros/point_cloud.h>
-#include <pcl/point_types.h>
+#include <pcl_ros/transforms.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/filters/voxel_grid.h>
 
@@ -18,17 +18,16 @@
 #include <moveit_msgs/DisplayRobotState.h>
 #include <moveit_msgs/DisplayTrajectory.h>
 
-ros::NodeHandle nh;
 bool detect_grasping_point;
 
 tf2_ros::Buffer tf_buffer;
-tf2_ros::TransformListener tf_listener;
+tf2_ros::TransformListener tf_listener(tf_buffer);
 
 //pre-grasp motion
 bool preGraspMovement()
 {
-    // Load Controller Configuration
-    nh.setParam("/move_group/controller_list", "config/simple_moveit_controllers.yaml");
+    // Load Controller Configuration : loaded in main function
+    //nh.setParam("/move_group/controller_list", "config/simple_moveit_controllers.yaml");
 
     // Planning group
     static const std::string PLANNING_GROUP = "panda_arm";
@@ -50,7 +49,6 @@ bool preGraspMovement()
     moveit::planning_interface::MoveGroupInterface::Plan pre_grasp_plan;
     bool success = static_cast<bool>(move_group.plan(pre_grasp_plan));
 
-    
     ros::Duration(5.0).sleep();
 
     if (success)
@@ -58,7 +56,7 @@ bool preGraspMovement()
         ROS_INFO("Pre-grasp plan successful. Executing the plan.");
 
         // Execute the plan
-        move_group.execute(my_plan);
+        move_group.execute(pre_grasp_plan);
         return true;
     }
     else
@@ -70,23 +68,29 @@ bool preGraspMovement()
 
 //point cloud sub callback
 
-void pointCloudCallback(const const sensor_msgs::PointCloud2ConstPtr& points_msg)
+void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& points_msg)
 {
     if(!detect_grasping_point) return;
         
     //convert to pcl type
-    PointCloud::Ptr pcl_cloud(new PointCloud());
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
     pcl::fromROSMsg(*points_msg, *pcl_cloud);
 
     //transform point cloud
-    PointCloud::Ptr transformed_cloud(new PointCloud());
-    pcl_ros::transformPointCloud(odom_child_frame_id, *pcl_cloud, *transformed_cloud, tf_buffer);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr transformed_cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+    pcl_ros::transformPointCloud("panda_link0", *pcl_cloud, *transformed_cloud, tf_buffer);
 
-    PointCloud::Ptr filtered_cloud(new PointCloud());
-    pcl::VoxelGrid<PointCloud> vox;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr filtered_cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
+    pcl::VoxelGrid<pcl::PointXYZRGB> vox;
     vox.setInputCloud(transformed_cloud);
     vox.setLeafSize(0.005f, 0.005f, 0.005f);
-    vox.filter (*cloud_filtered);
+    vox.filter (*filtered_cloud);
+
+    //publish new point cloud
+    ros::Publisher  base_pcl_pub = nh.advertise<sensor_msgs::PointCloud2>("/camera/transformed/color/points", 1);
+
+    filtered_cloud->header.frame_id = "panda_link0";    
+    base_pcl_pub.publish(filtered_cloud);
 
     // float max_height = -6.0;
 
@@ -98,12 +102,12 @@ void pointCloudCallback(const const sensor_msgs::PointCloud2ConstPtr& points_msg
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "cloth_manipulation_node");
+    nh.setParam("/move_group/controller_list", "config/simple_moveit_controllers.yaml");
+
+    ros::NodeHandle nh;
     
     ros::AsyncSpinner spinner(3);
     spinner.start();
-
-    tf_buffer();
-    tf_listener(tf_buffer);
 
     // bool pre_grasp_success = preGraspMovement();
 
@@ -115,8 +119,10 @@ int main(int argc, char **argv)
 
     detect_grasping_point = false;
 
-    ros::Subscriber pcl_sub = nh.subscribe<PointCloud>("/camera/depth/color/points", 1, pointCloudCallback); 
+    ros::Subscriber pcl_sub = nh.subscribe<sensor_msgs::PointCloud2>("/camera/depth/color/points", 1, pointCloudCallback); 
 
-    ros::shutdown();
-    return 0;
+    // ros::shutdown();
+    // return 0;
+
+    ros::spin ();
 }
