df4bad177a30e7204ad14678ffd355debe444667
Pranav Thakkar <pnt8@cornell.edu>
2024-05-27 16:30:08 -0400

    Add 2nd point input for visual policy

--- scripts/visual_policy.py
+++ scripts/visual_policy.py
@@ -28,7 +28,7 @@ class PixelSelector:
             cv2.circle(self.img, (x, y), 3, (200, 0, 0), -1)
             cv2.imshow("pixel_selector", self.img)
 
-    def run(self, img, num_clicks=1):
+    def run(self, img, num_clicks=2):
         self.load_image(img)
         self.clicks = []
         cv2.namedWindow('pixel_selector')
@@ -118,8 +118,8 @@ class RealSenseROS:
         cy = camera_info.K[5]  
 
         # Convert to world space
-        world_x = (depth / fx) * (image_x - cx)
-        world_y = (depth / fy) * (image_y - cy)
+        world_x = - (depth / fx) * (image_x - cx)
+        world_y = - (depth / fy) * (image_y - cy)
         world_z = depth
 
         return True, np.array([world_x, world_y, world_z])
@@ -158,18 +158,18 @@ class RealSenseROS:
         marker = Marker()
         marker.id = id
         marker.header.stamp = rospy.Time.now()
-        marker.header.frame_id = "camera_color_optical_frame"
-        marker.type = marker.CUBE
+        marker.header.frame_id = "panda_link0"
+        marker.type = marker.ARROW
         marker.action = marker.ADD
-        marker.scale.x = 0.05
+        marker.scale.x = 0.5
         marker.scale.y = 0.05
         marker.scale.z = 0.05
         marker.color.a = 1.0
 
         # marker color is red
-        marker.color.r = 1.0
+        marker.color.r = 0.5
         marker.color.g = 0.0
-        marker.color.b = 0.0
+        marker.color.b = 0.5
 
         # pose = self.get_pose_msg_from_transform(transform)
         marker.pose = pose
@@ -178,21 +178,32 @@ class RealSenseROS:
 
         self.point_visualization_pub.publish(marker_array)
 
-    def fixed_policy(self, pose):
+    def fixed_policy(self, pose_array):
 
         # Fixed policy : x, y are unchanged. z changes
         # pose.position.x += 0.1
         # pose.position.y += 0.1
 
+        pose_init = pose_array[0].pose
+        pose_fin = pose_array[1].pose
+
+        roll = 0
+        pitch = 0
+        yaw = math.atan2(pose_fin.position.y - pose_init.position.y, pose_fin.position.x - pose_init.position.x)
+
+        print("Yaw (deg): ", yaw * 180 / math.pi)
+
+        quat = Rotation.from_euler('zyx', [yaw, pitch, roll]).as_quat()
+
         pose_policy = Pose()
-        pose_policy.position.x = pose.position.x
-        pose_policy.position.y = pose.position.y
+        pose_policy.position.x = pose_init.position.x
+        pose_policy.position.y = pose_init.position.y
         pose_policy.position.z = 0.25
 
-        pose_policy.orientation.x = 0.0
-        pose_policy.orientation.y = 0.0
-        pose_policy.orientation.z = 0.0
-        pose_policy.orientation.w = 1.0
+        pose_policy.orientation.x = quat[0]
+        pose_policy.orientation.y = quat[1]
+        pose_policy.orientation.z = quat[2]
+        pose_policy.orientation.w = quat[3]       #scipy follows scalar-last convention for quaternions (source : https://docs.scipy.org/doc/scipy/reference/generated/scipy.spatial.transform.Rotation.from_quat.html)
 
         return pose_policy
 
@@ -205,39 +216,48 @@ if __name__ == "__main__":
 
         header, color_data, info_data, depth_data = rs_ros.get_camera_data()    
         pixel = pixel_selector.run(color_data)
-        print("Pixel: ",pixel)
+        print("Pixels: ",pixel)
 
-        valid, world_coordinate = rs_ros.pixel2World(info_data, pixel[0][0], pixel[0][1], depth_data)
+        valid = [False, False]
+        world_coordinate = [None, None]
+
+        for i in range(0,2):
+            valid[i], world_coordinate[i] = rs_ros.pixel2World(info_data, pixel[i][0], pixel[i][1], depth_data)
         
-        if valid:
-            point_transform = np.eye(4)
-            point_transform[:3,3] = world_coordinate.reshape(1,3)
+        print(valid)
+        pose_transformed = [PoseStamped(), PoseStamped()]
+        if all(valid):
+            for i in range(0,2):
+                point_transform = np.eye(4)
+                point_transform[:3,3] = world_coordinate[i].reshape(1,3)
 
-            # rs_ros.visualize_point(point_transform)
-            print("Camera Coordinate: ", world_coordinate)
+                # rs_ros.visualize_point(point_transform)
+                print("Camera Coordinate: ", world_coordinate[i])
 
-            pose = PoseStamped()
-            pose.pose = rs_ros.get_pose_msg_from_transform(point_transform)
-            pose.header.frame_id = "camera_color_optical_frame"
-            pose.header.stamp = rospy.Time.now()
+                pose = PoseStamped()
+                pose.pose = rs_ros.get_pose_msg_from_transform(point_transform)
+                pose.header.frame_id = "camera_color_optical_frame"
+                pose.header.stamp = rospy.Time.now()
 
-            try:
-                transform = rs_ros.tf_buffer.lookup_transform("panda_link0",
-                                           "camera_color_optical_frame",
-                                           rospy.Time.now(),
-                                           rospy.Duration(1.0))
-                pose_transformed = tf2_geometry_msgs.do_transform_pose(pose, transform)
-                print("Successfully transformed")
-                
-                pose_policy = rs_ros.fixed_policy(pose_transformed.pose)
-                rs_ros.visualize_point(pose_policy)
+                try:
+                    transform = rs_ros.tf_buffer.lookup_transform("panda_link0",
+                                               "camera_color_optical_frame",
+                                               rospy.Time.now(),
+                                               rospy.Duration(1.0))
+                    pose_transformed[i] = tf2_geometry_msgs.do_transform_pose(pose, transform)
+                    print("Successfully transformed point %s", i)
 
-                print("Pose for policy is: ", pose_policy)
+                except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException, tf2_ros.TransformException) as ex:
+                    rospy.logerr('Transformation error occurred: %s', ex)
 
-            except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException, tf2_ros.TransformException) as ex:
-                rospy.logerr('Transformation error occurred: %s', ex)
+            
+            pose_policy = rs_ros.fixed_policy(pose_transformed)
+            rs_ros.visualize_point(pose_policy)
+            print("Pose for policy is: ", pose_policy)
+
         else:
-            print("Pixel selected has invalid depth :(")
+            print("Some pixels selected have invalid depth :(")
+            print(valid)
 
         input("Press [ENTER] to move to try again ...")
         cv2.destroyAllWindows()
